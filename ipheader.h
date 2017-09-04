7d085f94ecbaf9abfd8a1e2f62510f88240b4944
Roman Lukomskyi <rlukomskyi@gmail.com>
2017-09-04 20:24:20 +0300

    Change IPHeader structure (remove Data field)

--- ipheader.h
+++ ipheader.h
@@ -7,18 +7,21 @@ using namespace std;
 
 class IpHeader
 {
-    unsigned char  ver_len_;
-    unsigned char  tos_;
-    unsigned short length_;
-    unsigned short id_;
-    unsigned short flgs_offset_;
-    unsigned char  ttl_;
-    unsigned char  protocol_;
-    unsigned short xsum_;
-    unsigned long  src_;
-    unsigned long  dest_;
-    unsigned short *params_;
-    unsigned char  *data_;
+    unsigned char header_len : 4;    //Length of header
+    unsigned char version : 4;       // IP protocol version
+    unsigned char tos;               // Type of service
+    unsigned short total_length;     // Total length
+    unsigned short id;               //ID of ip packet
+    unsigned char frag_offset : 5;   //Fragmentation offset
+    unsigned char more_fragment : 1; //More fragment
+    unsigned char dont_fragment : 1; //Dont fragment
+    unsigned char reserved_zero : 1; //Reserverd zero
+    unsigned char frag_offset1;      // Offset
+    unsigned char ttl;               // Time to live
+    unsigned char protocol;          // Underlaying protocol (TCP, UDP...)
+    unsigned short checksum;         //Checksum
+    unsigned int srcaddr;            //source address
+    unsigned int destaddr;           //destination addresss
 
     char ByteH(char c) { return c & 0xF; }
     char ByteL(char c) { return c >> 4; }
