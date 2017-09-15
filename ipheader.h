cd76dfa192dbe78c2008892b51511b56af2038d4
Roman Lukomskyi <romanluk@users.noreply.github.com>
2017-09-15 18:13:17 +0300

    Update ipheader.h

--- ipheader.h
+++ ipheader.h
@@ -8,21 +8,21 @@ using namespace std;
 
 class IpHeader
 {
-    unsigned char header_len_ : 4;    //Length of header
+    unsigned char header_len_ : 4;    // Length of header
     unsigned char version_ : 4;       // IP protocol version
     unsigned char tos_;               // Type of service
     unsigned short total_length_;     // Total length
-    unsigned short id_;               //ID of ip packet
-    unsigned char frag_offset_ : 5;   //Fragmentation offset
-    unsigned char more_fragment_ : 1; //More fragment
-    unsigned char dont_fragment_ : 1; //Dont fragment
-    unsigned char reserved_zero_ : 1; //Reserverd zero
+    unsigned short id_;               // ID of ip packet
+    unsigned char frag_offset_ : 5;   // Fragmentation offset
+    unsigned char more_fragment_ : 1; // More fragment
+    unsigned char dont_fragment_ : 1; // Dont fragment
+    unsigned char reserved_zero_ : 1; // Reserverd zero
     unsigned char frag_offset1_;      // Offset
     unsigned char ttl_;               // Time to live
     unsigned char protocol_;          // Underlaying protocol (TCP, UDP...)
-    unsigned short checksum_;         //Checksum
-    unsigned int srcaddr_;            //source address
-    unsigned int destaddr_;           //destination addresss
+    unsigned short checksum_;         // Checksum
+    unsigned int srcaddr_;            // Source address
+    unsigned int destaddr_;           // Destination addresss
 
     string UnsignedIpToString (unsigned int ip);
 
