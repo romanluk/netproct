71d24ed023f8e942462d1573c548d4069078f4a2
Roman Lukomskyi <rlukomskyi@gmail.com>
2017-10-04 21:07:30 +0300

    Add ip packet class

---
+++ ippacket.cpp
@@ -0,0 +1,6 @@
+#include "ippacket.h"
+
+IpPacket::IpPacket()
+{
+
+}
---
+++ ippacket.h
@@ -0,0 +1,11 @@
+#ifndef IPPACKET_H
+#define IPPACKET_H
+
+
+class IpPacket
+{
+public:
+    IpPacket();
+};
+
+#endif // IPPACKET_H
